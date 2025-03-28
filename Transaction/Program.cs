c3dd248ffe39a6bc2fd50965eb12e7ecc550b2fc
Selina Sun <selinayifan@gmail.com>
2025-03-27 22:50:23 -0400

    Added Delegates, Transaction, VisaAccount.

---
+++ Transaction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction
+{
+    public struct Transaction
+    {
+        // Properties with public getter and no setter
+        public string AccountNumber { get; }
+        public decimal Amount { get; }
+        public Person Originator { get; }
+        public DateTime Time { get; }
+
+        // Constructor
+        public Transaction(string accountNumber, decimal amount, Person person)
+        {
+            AccountNumber = accountNumber;
+            Amount = amount;
+            Originator = person;
+            Time = Util.Now; // Initialized with Util.Now property
+        }
+
+        // ToString override
+        public override string ToString()
+        {
+            string transactionType = Amount >= 0 ? "Deposit" : "Withdraw";
+            return $"{transactionType}: Account #{AccountNumber}, " +
+                   $"Name: {Originator.Name}, Amount: {Math.Abs(Amount):C}, Time: {Time}";
+        }
+    }
+}

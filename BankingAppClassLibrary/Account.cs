3b0216b063a33852a2e3496a40b8b4f181b4e5cf
Hojun <hojunkim105@gmail.com>
2025-03-30 17:49:08 -0400

    account.cs and checkingaccount.cs

--- BankingAppClassLibrary/Account.cs
+++ BankingAppClassLibrary/Account.cs
@@ -6,7 +6,73 @@ using System.Threading.Tasks;
 
 namespace BankingAppClassLibrary
 {
-    public class Account
+    public abstract class Account
     {
+        private static int LAST_NUMBER = 100000;
+        private readonly List<Person> users;
+        public readonly List<Transaction> transactions;
+        public event TransactionEventHandler OnTransaction;
+
+        public string Number { get; }
+        protected decimal Balance { get; set; }
+        protected decimal LowestBalance { get; set; }
+
+        public Account(string type, decimal balance)
+        {
+            Number = type + LAST_NUMBER;
+            LAST_NUMBER++;
+            Balance = balance;
+            LowestBalance = balance;
+            users = new List<Person>();
+            transactions = new List<Transaction>();
+        }
+
+        protected void Deposit(decimal amount, Person person)
+        {
+            Balance += amount;
+            if (Balance < LowestBalance)
+            {
+                LowestBalance = Balance;
+            }
+
+            Transaction transaction = new Transaction(Number, amount, person);
+            transactions.Add(transaction);
+        }
+
+        public void AddUser(Person user)
+        {
+            users.Add(user);
+        }
+
+        public bool IsUser(Person user)
+        {
+            foreach (var accUser in users)
+            {
+                if (accUser.Name == user.Name) return true;
+            }
+            return false;
+        }
+
+        public virtual void OnTransactionOccur(object sender, TransactionEventArgs e)
+        {
+            OnTransaction?.Invoke(sender, e);
+        }
+
+        public abstract void PrepareMonthlyStatement();
+
+        public override string ToString()
+        {
+            string result = $"Account Number: {Number}\nBalance: {Balance}\nUsers:\n";
+            foreach (var user in users)
+            {
+                result += $"- {user.Name}\n";
+            }
+            result += "Transactions:\n";
+            foreach (var transaction in transactions)
+            {
+                result += $"- {transaction}\n";
+            }
+            return result;
+        }
     }
-}
+    }
--- BankingAppClassLibrary/CheckingAccount.cs
+++ BankingAppClassLibrary/CheckingAccount.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace BankingAppClassLibrary
 {
-    internal class CheckingAccount
+    public class CheckingAccount : Account
     {
+        private const decimal COST_PER_TRANSACTION = 0.05m;
+        private const decimal INTEREST_RATE = 0.005m;
+        private bool hasOverdraft;
+
+        public CheckingAccount(decimal balance = 0, bool hasOverdraft = false)
+            : base("CK", balance) => this.hasOverdraft = hasOverdraft;
+
+        public new void Deposit(decimal amount, Person person)
+        {
+            base.Deposit(amount, person);
+            OnTransactionOccur(this, new TransactionEventArgs(person.Name, amount, true));
+        }
+
+        public void Withdraw(decimal amount, Person person)
+        {
+            if (!IsUser(person) || !person.IsAuthenticated)
+            {
+                OnTransactionOccur(this, new TransactionEventArgs(person.Name, amount, false));                
+            }
+
+            if (Balance - amount < 0 && !hasOverdraft)
+            {
+                OnTransactionOccur(this, new TransactionEventArgs(person.Name, amount, false));               
+            }
+
+            base.Deposit(-amount, person);
+            OnTransactionOccur(this, new TransactionEventArgs(person.Name, amount, true));
+        }
+
+        public override void PrepareMonthlyStatement()
+        {
+            Balance += (LowestBalance * INTEREST_RATE) / 12 - transactions.Count * COST_PER_TRANSACTION;
+            transactions.Clear();
+        }
     }
-}
+    }

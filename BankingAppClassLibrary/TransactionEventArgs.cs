58d9054a48286a303a81d46b6bb6261c24e8aace
eddie <Eddie000321@outlook.com>
2025-04-01 15:22:52 -0400

    delete // Delegate definitions public delegate void TransactionEventHandler(object sender, TransactionEventArgs e); public delegate void LoginEventHandler(object sender, LoginEventArgs e); in TransactionEventArgs.cs already declared in Delegates.cs

--- BankingAppClassLibrary/TransactionEventArgs.cs
+++ BankingAppClassLibrary/TransactionEventArgs.cs
@@ -16,8 +16,4 @@ namespace BankingAppClassLibrary
             Amount = amount;
         }
     }
-
-    // Delegate definitions
-    public delegate void TransactionEventHandler(object sender, TransactionEventArgs e);
-    public delegate void LoginEventHandler(object sender, LoginEventArgs e);
 }
